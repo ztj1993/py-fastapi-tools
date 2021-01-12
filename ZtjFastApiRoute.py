8de3c0dd2e2cf6e0e70e7f3f1ec145c3e8f26e15
Ztj <ztj1993@gmail.com>
2021-01-12 14:01:13 +0800

    优化 health 接口

--- ZtjFastApiRoute.py
+++ ZtjFastApiRoute.py
@@ -6,11 +6,12 @@
 # Date: 2020-01-03
 
 from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
 
 Router = APIRouter()
 
 
-@Router.get('/health')
+@Router.get('/health', response_class=PlainTextResponse)
 async def health():
     """服务状态"""
     return 'ok'
